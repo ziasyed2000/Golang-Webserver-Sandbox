70ebb2df9b0010b6523022c9ceccbfd5e1ca24e3
Zia Syed <ziasyed2000@gmail.com>
2023-03-17 01:08:14 -0400

    added article and result structs for data parsing

--- news/news.go
+++ news/news.go
@@ -1,6 +1,29 @@
 package news
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
+
+type Article struct {
+	Source struct {
+		ID   interface{} `json:"id"`
+		Name string      `json:"name"`
+	} `json:"source"`
+	Author      string    `json:"author"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	URL         string    `json:"url"`
+	URLToImage  string    `json:"urlToImage"`
+	PublishedAt time.Time `json:"publishedAt"`
+	Content     string    `json:"content"`
+}
+
+type Results struct {
+	Status       string    `json:"status"`
+	TotalResults int       `json:"totalResults"`
+	Articles     []Article `json:"articles"`
+}
 
 type Client struct {
 	http     *http.Client
