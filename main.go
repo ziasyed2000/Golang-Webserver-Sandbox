31bd92197dec4a0cc64c7422be313294976c8730
Zia Syed <ziasyed2000@gmail.com>
2023-03-17 01:07:04 -0400

    added usage of news endpoint and updated search handler

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/ziasyed2000/Golang-Webserver-Sandbox/news"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
@@ -17,22 +19,24 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
 }
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
-	u, err := url.Parse(r.URL.String())
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+func searchHandler(newsapi *news.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		u, err := url.Parse(r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-	params := u.Query()
-	searchQuery := params.Get("q")
-	page := params.Get("page")
-	if page == "" {
-		page = "1"
-	}
+		params := u.Query()
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
 
-	fmt.Println("Search Query is: ", searchQuery)
-	fmt.Println("Page is: ", page)
+		fmt.Println("Search Query is: ", searchQuery)
+		fmt.Println("Page is: ", page)
+	}
 }
 
 func main() {
@@ -46,11 +50,19 @@ func main() {
 		port = "3000"
 	}
 
+	apiKey := os.Getenv("NEWS_API_KEY")
+	if apiKey == "" {
+		log.Fatal("Env: apiKey must be set")
+	}
+
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	newsapi := news.NewClient(myClient, apiKey, 20)
+
 	fs := http.FileServer(http.Dir("assets"))
 
 	mux := http.NewServeMux()
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	mux.HandleFunc("/search", searchHandler)
+	mux.HandleFunc("/search", searchHandler(newsapi))
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
 }
